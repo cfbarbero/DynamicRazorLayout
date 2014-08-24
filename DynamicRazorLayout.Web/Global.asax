eefdaa10accc00a08dcdc88193720de4158ed8a8
Cris Barbero <cfbarbero@gmail.com>
2014-08-23 22:50:19 -0600

    Initial Commit

---
+++ DynamicRazorLayout.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DynamicRazorLayout.Web.MvcApplication" Language="C#" %>
---
+++ DynamicRazorLayout.Web/TestWebViewPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicRazorLayout.Web
+{
+    public abstract class TestWebViewPage<T> : WebViewPage<T>
+    {
+        public bool LayoutAssigned
+        {
+            get
+            {
+                return (ViewBag.LayoutAssigned == true);
+            }
+            set
+            {
+                ViewBag.LayoutAssigned = value;
+            }
+        }
+
+        public string LayoutName { get; set; }
+        public override void ExecutePageHierarchy()
+        {
+            if (!LayoutAssigned) { base.Layout = "~/Views/Shared/test.cshtml"; LayoutAssigned = true; }
+            base.ExecutePageHierarchy();
+        }
+    }
+    public abstract class TestWebViewPage : WebViewPage
+    {
+        public string LayoutName { get; set; }
+        
+
+    }
+}
